2cfcc19b68ff9ca56ff007d40aa4a64787ad232c
gadzhief <gadzhief@gmail.con>
2023-04-07 21:27:19 +0300

    added home controller

---
+++ src/main/java/com/gadzhief/springcloud/HomeController.java
@@ -0,0 +1,13 @@
+package com.gadzhief.springcloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping()
+    public String hello() {
+        return "index";
+    }
+}
